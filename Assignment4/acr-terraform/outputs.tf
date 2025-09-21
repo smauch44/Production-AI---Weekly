256689da12a8f63ea9df1834a5d3129e97d02276
smauch44 <stefan.mauch1993@gmail.com>
2025-09-21 08:07:07 +0200

    Copy acr-app and acr-terraform into Assignment4 folder

---
+++ Assignment4/acr-terraform/outputs.tf
@@ -0,0 +1,2 @@
+output "acr_name" { value = azurerm_container_registry.acr.name }
+output "acr_login_server" { value = azurerm_container_registry.acr.login_server }
---
+++ Assignment4/acr-terraform/variables.tf
@@ -0,0 +1,3 @@
+variable "resource_group_name" { type = string }
+variable "location" { type = string }
+variable "acr_name" { type = string } # must be lowercase; must be globally unique
